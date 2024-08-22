fbffafd41105477be6ab88d647e8e24cdb7573a7
soumya <mondalsoumya2001@gmail.com>
2024-08-22 14:44:05 +0530

    rollup config added

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import { terser } from 'rollup-plugin-terser'
+
+const devmode = (process.env.NODE_ENV !== 'production')
+console.log('devmode ', devmode)
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/index.js',
+      format: "es",
+      sourcemap: devmode ? 'inline' : false,
+      plugins: [
+        terser({
+          ecma: 2020,
+          mangle: { toplevel : true },
+          compress: {
+            module: true,
+            toplevel: true,
+            unsafe_arrows: true,
+            drop_console: !devmode,
+            drop_debugger: !devmode
+          },
+          output: { quote_style : 1 }
+        })
+      ]
+    }
+  }
+]
